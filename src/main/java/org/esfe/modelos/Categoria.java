9d70acb8920dc3b3f6544a3a444fd3934ae6dfe0
Orlando <sa23004@esfe.agape.edu.sv>
2024-08-23 14:37:54 -0600

    Funcionalidad completa para el modelo categoria

---
+++ src/main/java/org/esfe/modelos/Categoria.java
@@ -0,0 +1,43 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripcion es requerida")
+    private String descripcion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
